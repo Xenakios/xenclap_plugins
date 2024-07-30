4988dacfec2c28db5194177a7251e1aeaa5cd7b8
xenakios <xenakios@gmail.com>
2024-07-30 23:04:33 +0300

    Test program filter morph was wrong

--- source/main.cpp
+++ source/main.cpp
@@ -26,20 +26,22 @@ inline void test_klangas()
     as->m_shared_data.setPanMorphPreset(0);
     while (outcount < outlen)
     {
-        double val = 1.0 / outlen * outcount;
+        double val = 0.0 + 1.0 / outlen * outcount;
         val = std::clamp(val, 0.0, 1.0);
         for (auto &v : as->m_voices)
         {
             v.setNumPartials(32);
             v.setADSRParameters(0, 0.2, 0.2, 0.5, 0.2);
+            v.setADSRParameters(1, 0.2, 0.2, 0.5, 0.2);
             // v.setTuningMode(0);
             v.m_freq_tweaks_mode = 1;
-            v.m_freq_tweaks_mix = 0.1;
+            v.m_freq_tweaks_mix = val;
             v.m_adsr_burst_mix = 0.0;
             v.m_base_volume = -6.0;
-            v.m_filter_morph = 0.0;
+            v.m_filter_morph = 1.0;
             v.m_filter_mode = 0;
-            v.setPartialsBalance(val);
+            v.setPartialsBalance(1.0);
+            v.setPartialsPanMorph(0.0);
         }
 
         as->processBlock(procbuf.getView());
